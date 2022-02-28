77dab5674a1638da170ec5716207e57c0073f43a
Alex <webmaster@papagai.de>
2022-02-28 15:32:39 +0100

    create

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import valid from './modules/valid'
 
 
 
@@ -8,4 +9,5 @@ import modal from './modules/modal'
 timer('28 february 2022')
 menu()
 modal()
+valid()
 
---
+++ src/modules/valid.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const valid = () => {
+
+  const inputsText = document.querySelectorAll('input[type = text]')
+  const inputsEmail = document.querySelectorAll('input[type = email]')
+  const inputsTel = document.querySelectorAll('input[type = tel]')
+  const textarea = document.getElementById('form2-message')
+
+
+  inputsText.forEach((item) => {
+    item.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^\sа-яА-Я-]+/i, "")
+    })
+  })
+
+  textarea.addEventListener('input', (e) => {
+    e.target.value = e.target.value.replace(/[^\sа-яА-Я-]+/i, "")
+  })
+
+
+  inputsEmail.forEach((item) => {
+    item.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^\w@_\-.!~*']+/, "")
+    })
+  })
+
+  inputsTel.forEach((item) => {
+    item.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^\d()-]+/, "")
+    })
+  })
+
+ }
+
+export default valid;
